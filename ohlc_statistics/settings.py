c9000d867c9c1c439ff5bbc85b281ee5e78a26af
Msibragimov <msibragimov1@gmail.com>
2022-06-09 11:40:34 +0500

    added apikey secret

--- ohlc_statistics/settings.py
+++ ohlc_statistics/settings.py
@@ -117,12 +117,17 @@ REST_FRAMEWORK = {
 }
 
 
+# Drf API key
+
+API_KEY_SECRET = os.environ['API_KEY_SECRET']
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tashkent'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
